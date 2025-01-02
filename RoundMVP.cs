320b2e60c7cc6ad598e7a37e0d0bdd91e9a3ff48
Antoniofo <antonio57201@gmail.com>
2025-01-02 10:45:48 +0100

    bump

--- RoundMVP.cs
+++ RoundMVP.cs
@@ -1,40 +1,39 @@
 ﻿namespace RoundMVP
 {
-     using Exiled.API.Features;
+    using Exiled.API.Features;
 
-     public class Plugin : Plugin<Config>
-     {
-          public override string Name => "RoundMVP";
-          public override string Prefix => "RoundMVP";
-          public override string Author => "@misfiy";
-          public override Version RequiredExiledVersion => new(8, 2, 1);
-          public override Version Version => new(1, 2, 4);
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "RoundMVP";
+        public override string Prefix => "RoundMVP";
+        public override string Author => "@misfiy Modified by Antonionfo";
+        public override Version Version => new(1, 3, 0);
+        public override Version RequiredExiledVersion => new(9, 2, 1);
+        public static Plugin Instance;
+        private Handler handler;
 
-          public static Plugin Instance;
-          private Handler handler;
+        public override void OnEnabled()
+        {
+            Instance = this;
+            handler = new Handler();
+            Exiled.Events.Handlers.Player.Died += handler.OnDied;
+            Exiled.Events.Handlers.Player.Spawned += handler.OnSpawned;
+            Exiled.Events.Handlers.Server.WaitingForPlayers += handler.OnWaiting;
+            Exiled.Events.Handlers.Server.RoundEnded += handler.OnRoundEnd;
 
-          public override void OnEnabled()
-          {
-               Instance = this;
-               handler = new Handler();
-               Exiled.Events.Handlers.Player.Died += handler.OnDied;
-               Exiled.Events.Handlers.Player.Spawned += handler.OnSpawned;
-               Exiled.Events.Handlers.Server.WaitingForPlayers += handler.OnWaiting;
-               Exiled.Events.Handlers.Server.RoundEnded += handler.OnRoundEnd;
+            base.OnEnabled();
+        }
 
-               base.OnEnabled();
-          }
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.Died -= handler.OnDied;
+            Exiled.Events.Handlers.Player.Spawned -= handler.OnSpawned;
+            Exiled.Events.Handlers.Server.WaitingForPlayers -= handler.OnWaiting;
+            Exiled.Events.Handlers.Server.RoundEnded -= handler.OnRoundEnd;
 
-          public override void OnDisabled()
-          {
-               Exiled.Events.Handlers.Player.Died -= handler.OnDied;
-               Exiled.Events.Handlers.Player.Spawned -= handler.OnSpawned;
-               Exiled.Events.Handlers.Server.WaitingForPlayers -= handler.OnWaiting;
-               Exiled.Events.Handlers.Server.RoundEnded -= handler.OnRoundEnd;
-
-               handler = null!;
-               Instance = null!;
-               base.OnDisabled();
-          }
-     }
+            handler = null!;
+            Instance = null!;
+            base.OnDisabled();
+        }
+    }
 }
